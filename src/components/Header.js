b06f8a0a335ad0606b5382febd9de1955e52f03a
adanzan <danzanachiterdene@gmail.com>
2023-06-08 16:38:01 -0400

    Included picture, edited Hero CSS to experiment

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,13 @@ import { AppBar, Toolbar, Button } from '@mui/material';
 export default function Header() {
     return (
         <div>
-            <AppBar>
+            <AppBar position="fixed">
                 <Toolbar>
                     <Button variant="contained">Hello World</Button>
                     {/* Call to action -> View resume */}
                 </Toolbar>
             </AppBar>
+            <Toolbar/>
         </div>
-    )
+    );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,17 @@
-import * as React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
 
-import styles from '../styles/Hero.module.css'
+import styles from '../styles/Hero.module.css';
+import portrait from '../resources/images/portrait.jpeg';
 
 export default function Hero() {
     return (
         <div className={styles.Hero}>
-            {/* Two boxes (MUI?), one is picture, one is text */}
             <h2 className={styles.textSection}>Hi, I am Anthony Danzan Achit-Erdene</h2>
             {/* Could add one blurring out if one is hovered over - Anthony, Danzan */}
-            <h3 className={styles.imageSection}>Picture here </h3>
+            {/* <Box sx={{borderRadius: '16px'}}> */}
+            <img className={styles.imageSection} src={portrait} alt="Me"/>
+
         </div>
     )
 }
