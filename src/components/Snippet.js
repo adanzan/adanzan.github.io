f1cc045c267978206a80f89425b945c189648f4a
adanzan <danzanachiterdene@gmail.com>
2023-06-22 15:43:51 -0400

    Added paper for snippets, modified layout of all components

--- src/components/Snippet.js
+++ src/components/Snippet.js
@@ -3,6 +3,7 @@
 */
 
 import * as React from "react";
+import Paper from "@mui/material/Paper";
 
 import styles from "../styles/Snippet.module.css";
 
@@ -10,8 +11,8 @@ export default function Snippet ({ orgName, location, positions }) {
   // Creates an element for each position in the organization
   const positionElement = positions.map((position, index) => {
     return (
-            <div className="position" key={index}>
-                <div className="positionNameDate">
+            <div className={styles.position} key={index}>
+                <div className={styles.positionNameDate}>
                     <h3>{position.name}</h3>
                     <h3>{position.dateRange}</h3>
                 </div>
@@ -22,8 +23,10 @@ export default function Snippet ({ orgName, location, positions }) {
 
   return (
         <div className={styles.Snippet}>
+          <Paper className={styles.snippetPaper} variant="outlined">
             <h2>{orgName + " - " + location}</h2>
             {positionElement}
+          </Paper>
         </div>
   );
 }
