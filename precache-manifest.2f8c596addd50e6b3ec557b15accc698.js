deb1cf673e416b942c620c9c506ad6ba4bfa13ca
adanzan <danzanachiterdene@gmail.com>
2023-07-16 18:01:22 -0400

    Updates

---
+++ precache-manifest.2f8c596addd50e6b3ec557b15accc698.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2ad7310191dbb40010d",
+    "url": "/static/css/main.ca0adee7.chunk.css"
+  },
+  {
+    "revision": "b2ad7310191dbb40010d",
+    "url": "/static/js/main.adce4933.chunk.js"
+  },
+  {
+    "revision": "2542b6befa585976a9a7",
+    "url": "/static/js/runtime~main.1082e1f2.js"
+  },
+  {
+    "revision": "3e03241da7df32e57ac3",
+    "url": "/static/js/2.f6ae3f75.chunk.js"
+  },
+  {
+    "revision": "b002637b690670ff2901",
+    "url": "/static/js/3.dfe83729.chunk.js"
+  },
+  {
+    "revision": "9f538d0bde20f637522577236471c2c7",
+    "url": "/static/media/middleburyLogo.9f538d0b.png"
+  },
+  {
+    "revision": "889c0bd694edf4fdc600ec1f42d2555d",
+    "url": "/static/media/portrait.889c0bd6.png"
+  },
+  {
+    "revision": "22c5cfd8d2949fa711b389fa38db12c6",
+    "url": "/static/media/aesLogo.22c5cfd8.png"
+  },
+  {
+    "revision": "d0a1f0f3518945d8f4c3a1558eb05c11",
+    "url": "/static/media/agulaLogo.d0a1f0f3.jpeg"
+  },
+  {
+    "revision": "902956623e3c2bf43e7b4f9e1ffc4afe",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6bc56df99b89e07b88bb0f32004eedd1.js"
+  "/precache-manifest.2f8c596addd50e6b3ec557b15accc698.js"
 );
 
 workbox.clientsClaim();
