281d79dcbcb77540b2475088974cfc2082c9663b
adanzan <danzanachiterdene@gmail.com>
2023-07-29 13:45:48 -0400

    Update Skills tests

--- src/tests/Skills.test.js
+++ src/tests/Skills.test.js
@@ -4,7 +4,37 @@ import { render } from "@testing-library/react";
 
 import Skills from "../components/Skills"; 
 
-test("matches snapshot", () => {
-  const screenshotApp = render(<Skills />);
-  expect(screenshotApp).toMatchSnapshot();
+let testRender;
+
+describe("Skills component", () => {
+	beforeEach(() => {
+		testRender = render(<Skills />);
+	});
+	test("matches snapshot", () => {
+		expect(testRender).toMatchSnapshot();
+	});
+	test("has title", () => {
+		expect(testRender.getByText("SKILLS"));
+	});
+	describe("correctly displays", () => {
+		test("section titles", () => {
+			expect(testRender.getByText(/Programming.*Languages/));
+			expect(testRender.getByText("Tools"));
+			expect(testRender.getByText(/Frameworks.*Languages/));
+			expect(testRender.getByText("Soft Skills"));
+		});
+		describe("skill", () => {
+			test("logos", () => {
+				expect(testRender.getByTitle("Python logo"));
+				expect(testRender.getByTitle("Firebase logo"));
+				expect(testRender.getByTitle("React logo"));
+				expect(testRender.getByTitle("Agile"));
+			});
+			test("texts", () => {
+				expect(testRender.getByText("Python"));
+				expect(testRender.getByText("GitHub"));
+				expect(testRender.getByText("React"));
+			});
+		});
+	});
 });
