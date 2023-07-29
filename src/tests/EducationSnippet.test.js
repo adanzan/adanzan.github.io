acf6e8b49867936778ca8e9be9c185042288bdd7
adanzan <danzanachiterdene@gmail.com>
2023-07-29 12:57:10 -0400

    Update EducationSnippet tests

--- src/tests/EducationSnippet.test.js
+++ src/tests/EducationSnippet.test.js
@@ -4,16 +4,36 @@ import { render } from "@testing-library/react";
 
 import EducationSnippet from "../components/EducationSnippet"; 
 
-test("matches snapshot", () => {
-  const screenshotApp = render(<EducationSnippet 
-    name={"testName"}
-    degree={"testDegree"}
-    dateRange={"testDateRange"}
-    location={"testLocation"}
-    logo={"testLogo"}
-    link={"testLink"}
-    gpa={"testGpa"}
-    achievements={["testAchievement1, testAchievement2"]}
-    />);
-  expect(screenshotApp).toMatchSnapshot();
+let testRender;
+
+describe("EducationSnippet component", () => {
+	beforeEach(() => {
+		testRender = render(<EducationSnippet 
+			name={"testName"}
+			degree={"testDegree"}
+			dateRange={"testDateRange"}
+			location={"testLocation"}
+			logo={"testLogo"}
+			link={"testLink"}
+			gpa={"testGpa"}
+			achievements={["testAchievement1", "testAchievement2"]}
+		/>);
+	});
+	test("matches snapshot", () => {
+		expect(testRender).toMatchSnapshot();
+	});
+
+	describe("correctly displays", () => {
+		test("name", () => {
+			expect(testRender.getByRole("heading", "name")).toContainHTML("testName");
+		});
+		test("testGpa", () => {
+			expect(testRender.getByText("testGpa"));
+		});
+		test("achievements", () => {
+			expect(testRender.getByText("testAchievement1"));
+			expect(testRender.getByText("testAchievement2"));
+		});
+	});
 });
+
